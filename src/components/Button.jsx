d874f262b6c1600be5b5c0b91caa4a006995edda
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-04-04 23:43:58 -0700

    swapped link and div

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Button = ({ text, link }) => {
   return (
-    <div className="py-0.5 ring-2 ring-art-purple ring-offset-4 rounded-full drop-shadow-[4px_4px_1px_rgba(191,140,225,.70)]">
-      <Link
-        href={link}
-        className="py-1 px-16 bg-art-purple/35 ring-4 ring-offset-[2px] ring-offset-art-purple ring-art-purple/30 text-white text-xl font-bold rounded-full"
-      >
+    <Link
+      href={link}
+      className="ring-2 ring-art-purple ring-offset-4 rounded-full drop-shadow-[4px_4px_1px_rgba(191,140,225,.70)]"
+    >
+      <div className="py-1 px-8 md:px-16 bg-art-purple/35 ring-4 ring-offset-[2px] ring-offset-art-purple ring-art-purple/30 text-white text-sm md:text-xl font-bold rounded-full">
         {text}
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
