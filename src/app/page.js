f1a7cad91e73affc7aa83efc6a9928a72256598a
Xiyuan Wu <xwu136@gmail.com>
2024-04-04 21:29:14 -0700

    botton

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Landing from "@/components/Landing";
+import React from "react";
+import Button from "@/components/Button.jsx";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col items-center">
-      <Landing />
+      <p>Home</p>
+      <Button text="Click Me" link="/your-link" target="_self" />{" "}
+      {/* Add the Button component */}
     </div>
   );
 };
