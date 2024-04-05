374b0c13ebe50a7fc24916152052adb78dfefd5e
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-04-05 00:06:51 -0700

    added canvas

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,9 +5,9 @@ const Button = ({ text, link }) => {
   return (
     <Link
       href={link}
-      className="ring-2 ring-art-purple ring-offset-4 rounded-full drop-shadow-[4px_4px_1px_rgba(191,140,225,.70)]"
+      className="ring-2 ring-art-purple ring-offset-[5px] rounded-full drop-shadow-[4px_4px_1px_rgba(191,140,225,.70)]"
     >
-      <div className="py-1 px-8 md:px-16 bg-art-purple/35 ring-4 ring-offset-[2px] ring-offset-art-purple ring-art-purple/30 text-white text-sm md:text-xl font-bold rounded-full">
+      <div className="py-1 px-8 md:px-16 bg-art-purple/35 ring-[5px] ring-offset-[1.5px] ring-offset-art-purple ring-art-purple/30 text-white text-sm md:text-xl font-bold rounded-full">
         {text}
       </div>
     </Link>
