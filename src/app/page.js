b0617325fbf5a7c7594f54d465facfdfd441a177
Zarko36 <jmuni017@ucr.edu>
2024-04-05 22:41:48 -0700

    "Get Involved" done in landing

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "@/components/Button.jsx";
+import Landing from "@/components/Landing.jsx";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <p>Home</p>
       <Button text="LEARN MORE" link="/about" />
+      <Landing />
     </div>
   );
 };
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { FaDiscord } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
+import { FaLink } from "react-icons/fa";
+import { PiTiktokLogoBold } from "react-icons/pi";
 
 const Landing = () => {
   return (
-    <div>
-      <p>Landing</p>
+    <div className="flex flex-col items-center">
+      <p className="text-art-pink-200 text-lg font-bold ">Get Involved</p>
+      <hr className="border-2 border-art-pink-200 w-12" />
+      <div className="flex py-5 space-x-10">
+        <div className="flex flex-col items-center">
+          <FaDiscord size="48px" color="#E893C5" />
+          <p className="text-art-pink-200 text-sm md:text-sm font-bold ">
+            Discord
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <AiFillInstagram size="48px" color="#1C1DAB" />
+          <p className="text-art-blue-200 text-sm font-bold ">Instagram</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <MdEmail size="48px" color="#E893C5" />
+          <p className="text-art-pink-200 text-sm font-bold ">Email</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <PiTiktokLogoBold size="48px" color="#E893C5" />
+          <p className="text-art-pink-200 text-sm font-bold ">TikTok</p>
+        </div>
+        <div className="py-4 flex flex-col items-center">
+          <FaLink size="32px" color="#E893C5" />
+          <p className="text-art-pink-200 text-sm font-bold ">Highlander</p>
+        </div>
+      </div>
     </div>
   );
 };
