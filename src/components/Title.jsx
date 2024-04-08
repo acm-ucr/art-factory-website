bc3110bddcd3a4323f6e5927a5e434ae2cb39c49
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-04-08 15:51:29 -0700

    fixes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,14 +2,10 @@ import React from "react";
 
 const Title = ({ text }) => {
   return (
-    <div className="flex-col flex items-center">
-      <div
-        className={
-          "text-art-pink-100 text-center text-lg font-bold rounded-full font-urbanist"
-        }
-      >
+    <div className="flex-col flex">
+      <div className="text-art-pink-100 text-center text-3xl font-bold rounded-full font-urbanist">
         {text}
-        <div className="rounded bg-art-pink-100 w-5/3 h-1" />
+        <div className="flex rounded bg-art-pink-100 w-1/3 ml-14 h-1" />
       </div>
     </div>
   );
