f8237c5ee6fd4f504d60962f9b35f479d67a9fdf
Tokkia <hmiku4329@gmail.com>
2024-04-08 16:18:31 -0700

    added About component

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,31 +1,9 @@
 import React from "react";
-import Button from "@/components/Button";
+import About from "@/components/About";
 
 const page = () => {
   return (
-    <div className="w-full flex flex-col items-center text-center">
-      <p>About Us</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <Button text="LEARN MORE" link="/about" />
-      <p>What We Do</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-    </div>
+    <About />
   );
 };
 
