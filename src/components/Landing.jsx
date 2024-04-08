3cfe1695786852f11777a542de7571079e5ea0d6
Stanley Lew <stanley43595@gmail.com>
2024-04-07 21:27:51 -0700

    forgot to remove padding bc width was changed

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,9 +5,9 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div className="flex justify-center px-4 py-4">
+    <div className="flex justify-center py-4">
       <Image src={landingbg} alt="Landing Picture" className="w-5/6" />
-      <div className="absolute z-10 flex items-center justify-center mt-[13%] md:mt-[16%] px-8">
+      <div className="absolute z-10 flex items-center justify-center mt-[14.5%] md:mt-[16%] px-8">
         <Image src={landingtitle} alt="landingtitle" className="w-5/6" />
       </div>
     </div>
