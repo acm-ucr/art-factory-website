c593f6292854e26bb9b067bc6c0c88ddd22577b9
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-04-08 14:36:32 -0700

    Made the 5 different cards, but having trouble with the icons

--- src/components/ProjectType.jsx
+++ src/components/ProjectType.jsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 const ProjectType = ({ icon, title, description, className = " " }) => {
   return (
     <div
-      className={`font-poppins m-4 p-4 w-80 h-80 rounded-xl shadow flex flex-col items-center justify-between ${className}`}
+      className={`m-4 p-4 w-80 h-80 rounded-xl shadow flex flex-col items-center justify-center ${className}`}
     >
       <Image
         src={icon}
         alt="Icon"
-        className="w-3/4 h-3/4 rounded-xl object-cover"
+        className="w-1/8 h-1/8 rounded-xl object-cover -translate-y-1/4"
       />
       <div className="text-center">
-        <div className="mt-2 font-bold text-xl text-white bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
+        <div className="mt-2 font-bold text-xl -translate-y-3/4 text-white bg-clip-text">
           {title}
         </div>
         <div className="text-base mt-1 text-white">{description}</div>
