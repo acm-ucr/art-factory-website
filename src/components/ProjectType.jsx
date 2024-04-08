a4c5f2366bf4964b0e6488209bcb3dafa298ba6b
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-04-08 11:43:11 -0700

    Made the 5 different cards, but having trouble with the icons

--- src/components/ProjectType.jsx
+++ src/components/ProjectType.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
-const Card = ({ icon, title, desciption }) => {
+const ProjectType = ({ icon, title, description, className = " " }) => {
   return (
-    <div>
-      <p>Card</p>
+    <div
+      className={`font-poppins m-4 p-4 w-80 h-80 rounded-xl shadow flex flex-col items-center justify-between ${className}`}
+    >
+      <Image
+        src={icon}
+        alt="Icon"
+        className="w-3/4 h-3/4 rounded-xl object-cover"
+      />
+      <div className="text-center">
+        <div className="mt-2 font-bold text-xl text-white bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
+          {title}
+        </div>
+        <div className="text-base mt-1 text-white">{description}</div>
+      </div>
     </div>
   );
 };
 
-export default Card;
+export default ProjectType;
