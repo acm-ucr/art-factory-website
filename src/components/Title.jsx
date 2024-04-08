22b144b2ae22e744f2a4b468bb292615231d149e
unknown <aviambequette@gmail.com>
2024-04-08 02:30:53 -0700

    created initial Title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Title = ({ title }) => {
+const Title = ({ text }) => {
   return (
-    <div>
-      <p>Title</p>
+    <div className="flex-col flex items-center">
+      <div
+        className={
+          "text-art-pink-100 text-center text-lg font-bold rounded-full font-urbanist"
+        }
+      >
+        {text}
+        <div className="rounded bg-art-pink-100 w-5/3 h-1" />
+      </div>
     </div>
   );
 };
