e3ac81f5aac8fbb239b26417d1068c400568f980
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-04-08 15:12:13 -0700

    merge

--- src/components/ProjectType.jsx
+++ src/components/ProjectType.jsx
@@ -4,17 +4,16 @@ import Image from "next/image";
 const ProjectType = ({ icon, title, description, className = " " }) => {
   return (
     <div
-      className={`m-4 p-4 w-80 h-80 rounded-xl shadow flex flex-col items-center justify-center ${className}`}
+      className={`m-4 p-4 w-80 h-80 rounded-xl shadow flex flex-col items-center  gap-5 justify-center ${className}`}
     >
       <Image
         src={icon}
         alt="Icon"
-        className="w-1/8 h-1/8 rounded-xl object-cover -translate-y-1/4"
+        className="w-1/8 h-1/8 rounded-xl object-cover"
       />
-      <div className="text-center">
-        <div className="mt-2 font-bold text-xl -translate-y-3/4 text-white bg-clip-text">
-          {title}
-        </div>
+
+      <div className=" text-center mt-2 font-bold text-xl  text-white bg-clip-text">
+        {title}
         <div className="text-base mt-1 text-white">{description}</div>
       </div>
     </div>
