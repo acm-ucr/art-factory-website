7d627091f7e760a51494529acae2f7df5c72a1f1
Stanley Lew <stanley43595@gmail.com>
2024-04-07 19:48:04 -0700

    updated with two svgs, changed design a bit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,25 @@
 import landingbg from "../../public/landingbg.svg";
+import landingtitle from "../../public/landingtitle.svg";
 import Image from "next/image";
 import React from "react";
 
 const Landing = () => {
   return (
-    <div>
+    <div className="flex justify-center px-4 py-4">
       <Image
         src={landingbg.src}
-        alt="landingbg"
+        alt="Landing Picture"
         width={1000}
-        height={800}
-        className="pt-4 px-8"
+        height={1000}
       />
+      <div className="absolute z-10 flex items-center justify-center mt-[13%] px-8">
+        <Image
+          src={landingtitle.src}
+          alt="landingtitle"
+          width={760}
+          height={760}
+        />
+      </div>
     </div>
   );
 };
