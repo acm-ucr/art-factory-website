e3d2c2766ccd0309c5b2ca1e8a2560929a839b52
Xiyuan Wu <xwu136@gmail.com>
2024-04-10 19:38:31 -0700

    change botton opacity to 50

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ text, link }) => {
       href={link}
       className="ring-1 ring-art-purple ring-offset-[5px] rounded-full drop-shadow-[4px_4px_1px_rgba(191,140,225,.70)]"
     >
-      <div className="py-1 px-8 md:px-12 bg-art-purple/30 ring-4 ring-offset-[1.5px] ring-offset-art-purple ring-art-purple/30 text-white text-sm md:text-lg font-bold rounded-full">
+      <div className="py-1 px-8 md:px-12 bg-art-purple/50 ring-4 ring-offset-[1.5px] ring-offset-art-purple ring-art-purple/30 text-white text-sm md:text-lg font-bold rounded-full">
         {text}
       </div>
     </Link>
