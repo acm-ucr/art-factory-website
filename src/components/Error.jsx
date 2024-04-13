e883d3c5d502211c9ec46e42bdf54a671860e44a
Stanley Lew <stanley43595@gmail.com>
2024-04-12 21:45:44 -0700

    new colors, error page, button

---
+++ src/components/Error.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Button from "./Button";
+import errorclouds from "../../public/errorclouds.svg";
+import oopseasel from "../../public/oopseasel.svg";
+const Error = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-fit mx-8 mt-28">
+      <div className="grid grid-cols-2 gap-2">
+        <div>
+          <Image src={oopseasel} alt="oopseasel" className="w-10/12" />
+        </div>
+        <div className="flex-col items-center text-center ">
+          <div className="flex-col inline-flex">
+            <div className="flex justify-center bg-clip-text text-transparent bg-gradient-to-r from-art-pink-300 to-art-purple-200 text-2xl md:text-4xl font-bold ">
+              404
+            </div>
+            <div className="mt-2 w-20 h-1 bg-art-pink-300 rounded-lg" />
+          </div>
+          <p className="text-art-purple-100 text-center text-2xl font-bold pt-4">
+            Page Not Found
+          </p>
+          <p className="pt-4 pb-8">
+            Oh no! It looks like you stumbled upon our blank canvas – seems our
+            latest masterpiece is a work in progress! Our creative team is hard
+            at work crafting the perfect page for you. In the meantime, feel
+            free to explore our other completed works instead!
+          </p>
+          <Button text="BACK TO HOME" link="/" className="w-3/12 z-50" />
+        </div>
+      </div>
+      <Image
+        src={errorclouds}
+        alt="Error Clouds"
+        className=" absolute top-[25%] z-[-1]"
+      />
+    </div>
+  );
+};
+
+export default Error;
