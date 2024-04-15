25b8923be314a10f44789aa97a43781f83570596
Zarko36 <jmuni017@ucr.edu>
2024-04-15 12:03:19 -0700

    Renaming gallerycard parameter

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,12 +1,12 @@
 import React from "react";
 import GalleryCards from "@/components/GalleryCards";
-import { cards as data1 } from "@/data/photoSet1.js";
+import { cards as photoSet1 } from "@/data/photoSet1.js";
 
 const page = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <p>Gallery</p>
-      <GalleryCards cards={data1} />
+      <GalleryCards cards={photoSet1} />
     </div>
   );
 };
