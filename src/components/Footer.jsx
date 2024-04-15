0433005b49ddccc18bc8f23b36615298a3680787
liamregina <liam.xavier.regina@gmail.com>
2024-04-15 14:41:36 -0700

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+// import Image from "@/public/logo.svg";
+import { FaDiscord } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { IoMailOutline } from "react-icons/io5";
+import { RiTiktokLine } from "react-icons/ri";
 
 const Footer = () => {
   return (
     <div>
-      <p>Footer</p>
+      <FaDiscord /> <FaInstagram /> <IoMailOutline /> <RiTiktokLine />
+      <div className="flex justify-between bg-art-purple p-5">
+        <div className="flex gap-3">
+          <p>logo</p>
+          <p>Art Factory</p>
+        </div>
+        <div>icons</div>
+      </div>
     </div>
   );
 };
