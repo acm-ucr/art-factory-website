1be246661690538c465ef2362057a3fcb5ccc8d8
Zarko36 <jmuni017@ucr.edu>
2024-04-15 11:37:43 -0700

    Cleaning up

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,14 +1,12 @@
 import React from "react";
 import GalleryCards from "@/components/GalleryCards";
 import { cards as data1 } from "@/data/photoSet1.js";
-import { cards as data2 } from "@/data/photoSet2.js";
 
 const page = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <p>Gallery</p>
       <GalleryCards cards={data1} />
-      <GalleryCards cards={data2} />
     </div>
   );
 };
