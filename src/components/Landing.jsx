cd00d31a389face6f5f87b4497a3cf689b71b16a
Stanley Lew <stanley43595@gmail.com>
2024-04-16 02:48:05 -0700

    commit so i can pull from dev

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
   return (
     <div className="flex justify-center py-4">
       <Image src={landingbg} alt="Landing Picture" className="w-5/6" />
-      <div className="absolute z-10 flex items-center justify-center mt-[14.5%] md:mt-[16%] px-8">
+      <div className="absolute z-10 flex items-center justify-center mt-[14.5%] md:mt-[16%] 2xl:mt-[12%] px-8">
         <Image src={landingtitle} alt="landingtitle" className="w-5/6" />
       </div>
     </div>
