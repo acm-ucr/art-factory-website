8f8653ab1da631b5aea0f1282128b471bacbd8a5
Stanley Lew <stanley43595@gmail.com>
2024-04-17 15:38:20 -0700

    fixed board cards and button shadow

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Card from "@/components/board/Card";
+import Cards from "@/components/board/Cards";
 import Title from "@/components/Title";
 
 const page = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <Title text={"Meet the Board"} />
-      <Card />
+      <Cards />
     </div>
   );
 };
