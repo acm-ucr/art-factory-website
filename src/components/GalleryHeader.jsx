b218424481cc0449948838de1659397dcf0fd9e7
Avia Bequette <aviabequette@Avias-MBP.lan>
2024-04-17 18:15:43 -0700

    header

--- src/components/GalleryHeader.jsx
+++ src/components/GalleryHeader.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const GalleryHeader = ({ text }) => {
   return (
-    <div className="flex items-center justify-center">
-      <div className="border-[5.5px] border-art-purple rounded-full md:py-[2px] pt-[1.25px] pb-[1.25px] drop-shadow-[4px_4px_0px_rgba(191,140,225,.70)]">
-        <div className="py-1 px-14 sm:px-20 md:px-32 bg-art-purple-300 ring-4 ring-offset-[1.5px] ring-art-pink-400 ring-offset-art-purple  text-white text-sm md:text-2xl font-bold rounded-full">
-          {text}
+    <div className="flex items-center justify-center py-2">
+      <div className="border-4 border-art-purple rounded-full ring-1 ring-art-purple ring-offset-[1px] w-54 md:w-104 lg:w-104 lg:h-12 drop-shadow-[4px_4px_0px_rgba(191,140,225,.85)]">
+        <div className="py-1 w-48 md:w-96 lg:w-96 bg-art-purple-300 ring-4 ring-offset-[1px] ring-art-pink-400 ring-offset-art-purple rounded-full">
+          <div className="flex items-center justify-center px-0.5">
+            <div className="text-white text-sm md:text-2xl font-bold">
+              {text}
+            </div>
+          </div>
         </div>
       </div>
     </div>
