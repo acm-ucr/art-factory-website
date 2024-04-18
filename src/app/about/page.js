1847414431bc26105a25cdf364e3a50c9bd53dfa
unknown <aviambequette@gmail.com>
2024-04-17 22:35:49 -0700

    gallery headers in about (REMOVE)

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,13 @@
 import About from "@/components/about/About";
 import React from "react";
-
+import GalleryHeader from "@/components/GalleryHeader";
 const page = () => {
   return (
     <div className="flex flex-col items-center">
       <About />
+      <GalleryHeader text="Clay" />
+      <GalleryHeader text="Crafts" />
+      <GalleryHeader text="Studio Time" />
     </div>
   );
 };
