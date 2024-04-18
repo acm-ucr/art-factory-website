1ac29e38cfafaad4536fb604fcbaa8df31675e70
unknown <aviambequette@gmail.com>
2024-04-17 17:55:57 -0700

    responsiveness and formatted checked

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,9 +1,13 @@
 import React from "react";
+import GalleryHeader from "@/components/GalleryHeader";
 
 const page = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <p>Gallery</p>
+      <GalleryHeader text="Clay" />
+      <GalleryHeader text="Crafts" />
+      <GalleryHeader text="Studio Time" />
     </div>
   );
 };
