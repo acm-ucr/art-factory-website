44bd541e27402e6f1ad32bbb9bdcebd5655f3b9d
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-04-22 08:05:13 -0700

    update component

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,12 +1,15 @@
 import React from "react";
 import GalleryCards from "@/components/gallery/GalleryCards";
 import * as images from "@/data/images.js";
+// import GalleryLanding from "@/components/gallery/GalleryLanding";
 
 const page = () => {
   return (
-    <div className="w-full flex flex-col items-center">
-      <p>Gallery</p>
-      <GalleryCards cards={images.clay} />
+    <div className="w-full flex flex-col items-center gap-16">
+      {/* <GalleryLanding /> */}
+      <GalleryCards cards={images.clay} title={"Clay"} />
+      <GalleryCards cards={images.clay} title={"Crafts"} />
+      <GalleryCards cards={images.clay} title={"Studio Time"} />
     </div>
   );
 };
--- src/components/gallery/GalleryCards.jsx
+++ src/components/gallery/GalleryCards.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { FaAngleRight } from "react-icons/fa";
 import { FaAngleLeft } from "react-icons/fa";
 
-const GalleryCards = ({ cards }) => {
+const GalleryCards = ({ cards, title }) => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
   const handleLeftClick = () => {
@@ -20,37 +20,37 @@ const GalleryCards = ({ cards }) => {
   };
 
   return (
-    <div
-      style={{ display: "flex", flexWrap: "nowrap" }}
-      className="w-4/6 flex justify-center items-center w-full"
-    >
-      <button
-        onClick={handleLeftClick}
-        className="flex justify-center items-center"
-      >
-        <FaAngleLeft className="text-5xl text-art-pink-400 hover:text-art-purple-300" />
-      </button>
-      <Image
-        src={cards[currentCardIndex].image}
-        alt="Image"
-        className="w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100"
-      />
-      <Image
-        src={cards[currentCardIndex + 1].image}
-        alt="Image"
-        className="ml-4 w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100 hidden lg:block"
-      />
-      <Image
-        src={cards[currentCardIndex + 2].image}
-        alt="Image"
-        className="ml-4 w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100 hidden sm:block"
-      />
-      <button
-        onClick={handleRightClick}
-        className="flex justify-center items-center"
-      >
-        <FaAngleRight className="text-5xl text-art-pink-400 hover:text-art-purple-300" />
-      </button>
+    <div className=" flex justify-center flex-col items-center w-full gap-6 mb-3">
+      {/* <GalleryHeader text={title} /> */}
+      <div className="flex w-full justify-center gap-10 mt-2">
+        <button
+          onClick={handleLeftClick}
+          className="flex justify-center items-center"
+        >
+          <FaAngleLeft className="text-5xl text-art-pink-400 hover:text-art-purple-300" />
+        </button>
+        <Image
+          src={cards[currentCardIndex].image}
+          alt="Image"
+          className="w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100"
+        />
+        <Image
+          src={cards[currentCardIndex + 1].image}
+          alt="Image"
+          className="ml-4 w-60 h-60 rounded-xl justify-center items-center bg-art-purple-100 hidden lg:block"
+        />
+        <Image
+          src={cards[currentCardIndex + 2].image}
+          alt="Image"
+          className="ml-4 w-60 h-60 rounded-xl justify-center items-center bg-art-purple-100 hidden sm:block"
+        />
+        <button
+          onClick={handleRightClick}
+          className="flex justify-center items-center"
+        >
+          <FaAngleRight className="text-5xl text-art-pink-400 hover:text-art-purple-300" />
+        </button>
+      </div>
     </div>
   );
 };
