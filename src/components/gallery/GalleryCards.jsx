87667775ea38b8dcc46bc9569492beaaf5b0cbb8
Joel Muniz <Jmuni017@ucr.edu>
2024-04-21 18:03:06 -0700

    Implemented mobile responsiveness

--- src/components/gallery/GalleryCards.jsx
+++ src/components/gallery/GalleryCards.jsx
@@ -20,37 +20,37 @@ const GalleryCards = ({ cards }) => {
   };
 
   return (
-    <div>
-      <div
-        style={{ display: "flex", flexWrap: "nowrap" }}
-        className="w-full flex justify-center items-center flex-wrap w-full"
+    <div
+      style={{ display: "flex", flexWrap: "nowrap" }}
+      className="w-4/6 flex justify-center items-center w-full"
+    >
+      <button
+        onClick={handleLeftClick}
+        className="flex justify-center items-center"
       >
-        <button onClick={handleLeftClick}>
-          <div className="text-5xl text-art-pink-400 hover:text-art-purple-300">
-            <FaAngleLeft />
-          </div>
-        </button>
-        <div className="m-4 w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100">
-          <div className="flex items-center justify-center">
-            <Image src={cards[currentCardIndex].image} alt="Image" />
-          </div>
-        </div>
-        <div className="m-4 w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100 hidden sm:block">
-          <div className="flex items-center justify-center">
-            <Image src={cards[currentCardIndex + 1].image} alt="Image" />
-          </div>
-        </div>
-        <div className="m-4 w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100 hidden md:block">
-          <div className="flex items-center justify-center">
-            <Image src={cards[currentCardIndex + 2].image} alt="Image" />
-          </div>
-        </div>
-        <button onClick={handleRightClick}>
-          <div className="text-5xl text-art-pink-400 hover:text-art-purple-300">
-            <FaAngleRight />
-          </div>
-        </button>
-      </div>
+        <FaAngleLeft className="text-5xl text-art-pink-400 hover:text-art-purple-300" />
+      </button>
+      <Image
+        src={cards[currentCardIndex].image}
+        alt="Image"
+        className="w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100"
+      />
+      <Image
+        src={cards[currentCardIndex + 1].image}
+        alt="Image"
+        className="ml-4 w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100 hidden lg:block"
+      />
+      <Image
+        src={cards[currentCardIndex + 2].image}
+        alt="Image"
+        className="ml-4 w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100 hidden sm:block"
+      />
+      <button
+        onClick={handleRightClick}
+        className="flex justify-center items-center"
+      >
+        <FaAngleRight className="text-5xl text-art-pink-400 hover:text-art-purple-300" />
+      </button>
     </div>
   );
 };
