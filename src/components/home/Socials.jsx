c4cc3c09ec3d0a9abe18646c426bbcdf7150a39a
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-04-21 23:20:45 -0700

    fixed merge

--- src/components/home/Socials.jsx
+++ src/components/home/Socials.jsx
@@ -7,11 +7,11 @@ const Socials = () => {
     <div className="w-5/6">
       <div className="flex flex-col items-center">
         <Title text="Get Involved" />
-        <div className="container mx-auto flex justify-around">
+        <div className="gap-x-24 mx-auto flex justify-between ">
           {socials.map((social, index) => (
             <div
               key={index}
-              className="flex flex-col items-center object-contain text-5xl lg:text-7xl text-art-pink-100 hover:text-art-purple-100"
+              className="flex flex-col items-center text-5xl lg:text-7xl text-art-pink-100 hover:text-art-purple-100"
             >
               {social.icon}
               <p className="text-base font-bold">{social.name}</p>
