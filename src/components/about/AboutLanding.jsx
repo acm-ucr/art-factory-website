be7ee6dbcb633e6458953e1c8873798d202f62c8
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-04-22 07:34:54 -0700

    fixed color

--- src/components/about/AboutLanding.jsx
+++ src/components/about/AboutLanding.jsx
@@ -13,11 +13,11 @@ const AboutLanding = () => {
         className="absolute -left-[9%] top-[67%] w-64 rotate-3"
       />
 
-      <div className="text-purple-400 text-6xl tracking-[25px] drop-shadow-xl font-bold mt-20 ">
+      <div className="bg-gradient-to-r from-art-purple-200 to-art-pink-100 bg-clip-text text-transparent text-6xl tracking-[25px] drop-shadow-xl font-bold mt-20 p-1">
         Art Factory
       </div>
 
-      <div className="mt-3 flex items-center bg-indigo-500 rounded-full px-3 w-96 h-9 ml-10">
+      <div className="mt-3 flex items-center bg-art-purple-100 rounded-full px-3 w-96 h-9 ml-10">
         <Image src={logo} alt="landingLogo" className=" w-10 mt-3" />
         <p className="text-white tracking-[2px] mx-auto">
           {" "}
@@ -28,7 +28,7 @@ const AboutLanding = () => {
       <Image
         src={right}
         alt="rightring"
-        className="absolute -right-[8%] top-[30%] w-64 rotate-3"
+        className="absolute -right-[7%] top-[32%] w-64 rotate-0"
       />
     </div>
   );
