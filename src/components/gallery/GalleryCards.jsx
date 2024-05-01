17f3c207bcdb3e2e8857f90a5af7ae4538dbbb7e
Joel Muniz <Jmuni017@ucr.edu>
2024-04-30 20:26:48 -0700

    Adjusting gallerycards

--- src/components/gallery/GalleryCards.jsx
+++ src/components/gallery/GalleryCards.jsx
@@ -23,33 +23,33 @@ const GalleryCards = ({ cards, title }) => {
   return (
     <div className=" flex justify-center flex-col items-center w-full gap-6 mb-3">
       <GalleryHeader text={title} />
-      <div className="flex w-full justify-center gap-10 mt-2">
+      <div className="flex w-full justify-center justify-between mt-2">
         <button
           onClick={handleLeftClick}
           className="flex justify-center items-center"
         >
-          <FaAngleLeft className="text-5xl text-art-pink-400 hover:text-art-purple-300" />
+          <FaAngleLeft className="text-8xl text-art-pink-400 hover:text-art-purple-300" />
         </button>
         <Image
           src={cards[currentCardIndex].image}
           alt="Image"
-          className="w-60 h-60 rounded-xl flex justify-center items-center bg-art-purple-100"
+          className="w-80 h-80 rounded-xl flex justify-center items-center bg-art-purple-100"
         />
         <Image
           src={cards[currentCardIndex + 1].image}
           alt="Image"
-          className="ml-4 w-60 h-60 rounded-xl justify-center items-center bg-art-purple-100 hidden lg:block"
+          className="ml-4 w-80 h-80 rounded-xl justify-center items-center bg-art-purple-100 hidden lg:block"
         />
         <Image
           src={cards[currentCardIndex + 2].image}
           alt="Image"
-          className="ml-4 w-60 h-60 rounded-xl justify-center items-center bg-art-purple-100 hidden sm:block"
+          className="ml-4 w-80 h-80 rounded-xl justify-center items-center bg-art-purple-100 hidden md:block"
         />
         <button
           onClick={handleRightClick}
           className="flex justify-center items-center"
         >
-          <FaAngleRight className="text-5xl text-art-pink-400 hover:text-art-purple-300" />
+          <FaAngleRight className="text-8xl text-art-pink-400 hover:text-art-purple-300" />
         </button>
       </div>
     </div>
