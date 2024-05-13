57516a6fd41ca01ff7d754a282f42fe56f3f4aea
Stanley Lew <stanley43595@gmail.com>
2024-05-13 13:27:21 -0700

    wip ill get back to it later

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,9 +1,22 @@
+"use client";
 import landingbg from "@/public/landing/landingbg.svg";
 import leftcurve from "@/public/landing/leftcurve.svg";
 import rightcurve from "@/public/landing/rightcurve.svg";
 import logo from "@/public/LogoLanding.svg";
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
+
+const animations = {
+  start: {
+    opacity: 0,
+    y: -30,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 const Landing = () => {
   return (
@@ -13,12 +26,18 @@ const Landing = () => {
         <Image
           src={leftcurve}
           alt="leftring"
-          className="absolute left-[4%] md:-left-[3%] top-[66%] md:top-[80%] w-28 md:w-64 rotate-2 md:rotate-5"
+          className="absolute left-[4%] md:-left-[3%] top-[66%] md:top-[80%] w-28 md:w-64 rotate-2 md:rotate-5 animate-pulse"
         />
 
-        <div className="text-white font-lora text-2xl md:text-6xl tracking-[10px] md:tracking-[25px] drop-shadow-xl font-bold p-1">
+        <motion.div
+          className="text-white font-lora text-2xl md:text-6xl tracking-[10px] md:tracking-[25px] drop-shadow-xl font-bold p-1"
+          variants={animations}
+          initial="start"
+          whileInView="end"
+          transition={{ delay: 0.2 }}
+        >
           art factory
-        </div>
+        </motion.div>
 
         <div className="absolute mt-[16%] md:mt-[18%] flex items-center bg-art-purple-100 rounded-full px-3 w-54 md:w-8/12 h-[17px] md:h-10 ml-6">
           <Image
@@ -35,7 +54,7 @@ const Landing = () => {
         <Image
           src={rightcurve}
           alt="rightring"
-          className="absolute right-[2%] md:-right-[4%] mt-2 md:mt-3 w-28 md:w-5/12 -rotate-2"
+          className="absolute right-[2%] md:-right-[4%] mt-2 md:mt-3 w-28 md:w-5/12 -rotate-2 animate-pulse"
         />
       </div>
     </div>
