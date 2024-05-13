e2a811da3df70f5fecd2968a249ef06634321ac6
kavinphab <kavinphab@gmail.com>
2024-05-13 13:47:39 -0700

    attempt to add collapse

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({ text }) => {
   return (
     <div className="flex-col inline-flex gap-3 items-center justify-center">
-      <div className="text-art-pink-100  text-2xl md:text-4xl p-1 font-bold">
+      <div className="text-art-pink-100  text-2xl md:text-xl p-1 font-bold">
         {text}
         <div className="flex justify-center">
           <div className="mt-1 w-12 h-1.5 bg-art-pink-100 rounded-lg" />
