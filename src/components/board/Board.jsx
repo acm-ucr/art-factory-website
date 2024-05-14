e14f2b8d5e49974b730a9bcdd3d9c6bf30e3fb87
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-05-14 13:52:43 -0700

    updated

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,7 +1,25 @@
+"use client";
 import Image from "next/image";
-const Board = ({ name, title, image }) => {
+import { motion } from "framer-motion";
+
+const Board = ({ name, title, image, custom }) => {
+  const cardVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  const imageVariants = {
+    hover: { scale: 1.1 },
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center text-center hover:scale-105 transition-transform duration-300">
+    <motion.div
+      className="flex flex-col items-center justify-center text-center"
+      initial="hidden"
+      animate="visible"
+      variants={cardVariants}
+      transition={{ duration: 0.8, delay: custom }}
+    >
       <div className="w-[70%] translate-y-1/2 border-[1px] border-art-purple bg-white rounded-full text-art-purple text-sm md:text-lg font-semibold z-10 flex justify-center">
         {title}
       </div>
@@ -10,10 +28,19 @@ const Board = ({ name, title, image }) => {
           {name}
         </div>
       </div>
-      <div className="m-4 p-2 md:p-3 rounded-xl shadow-md bg-art-purple-100">
-        <Image src={image} alt={"profilepic"} className="rounded-lg" />
-      </div>
-    </div>
+      <motion.div
+        className="m-4 p-2 md:p-3 rounded-xl shadow-md bg-art-purple-100"
+        whileHover="hover"
+        variants={imageVariants}
+      >
+        <Image
+          src={image}
+          alt="profilepic"
+          className="rounded-lg"
+          layout="responsive"
+        />
+      </motion.div>
+    </motion.div>
   );
 };
 
--- src/components/board/Boards.jsx
+++ src/components/board/Boards.jsx
@@ -1,16 +1,17 @@
 import Card from "./Board";
-import { Board } from "../../data/board.js";
+import { Board as BoardData } from "../../data/board.js";
 
 const Boards = () => {
   return (
-    <div className="w-5/6">
+    <div className="w-5/6 mx-auto">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-        {Board.map((CARD, index) => (
+        {BoardData.map((CARD, index) => (
           <Card
             name={CARD.name}
             title={CARD.title}
             image={CARD.image}
             key={index}
+            custom={index * 0.1}
           />
         ))}
       </div>
