74759ef76ab57c32c95ef124b4a8c2e4577697a0
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-05-20 15:47:26 -0700

    ajusted gallery landing

--- src/components/gallery/GalleryLanding.jsx
+++ src/components/gallery/GalleryLanding.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import right from "@/public/aboutlanding/rightring.svg";
-import left from "@/public/aboutlanding/leftring.svg";
+import left from "@/public/right.svg";
 import logo from "@/public/LogoLanding.svg";
 
 const GalleryLanding = () => {
@@ -10,13 +10,13 @@ const GalleryLanding = () => {
       <Image
         src={left}
         alt="leftring"
-        className="absolute -left-[9%] top-[67%] w-64 rotate-3"
+        className="absolute -left-[6.5%] top-[67%] w-64 rotate-0"
       />
 
-      <div className="bg-gradient-to-r from-art-purple-200 to-art-pink-100 bg-clip-text text-transparent text-6xl tracking-[25px] drop-shadow-xl font-bold mt-20 p-1">
+      <div className="bg-gradient-to-r from-art-purple-200 to-art-pink-100 bg-clip-text text-transparent text-6xl tracking-[23px] drop-shadow-xl font-bold mt-20 p-1">
         Art Factory
       </div>
-      <div className="mt-3 ml-40 flex items-center bg-art-purple-100 rounded-full px-12 w-72 h-9">
+      <div className="mt-3 ml-40 flex items-center bg-art-purple-100 rounded-full px-2 w-64 h-9">
         <Image src={logo} alt="landingLogo" className=" w-10 mt-3" />
         <p className="text-white tracking-[2px] mx-auto"> photo gallery</p>
       </div>
@@ -24,7 +24,7 @@ const GalleryLanding = () => {
       <Image
         src={right}
         alt="rightring"
-        className="absolute -right-[7%] top-[32%] w-64 rotate-0"
+        className="absolute -right-[9%] top-[32%] w-64 rotate-0"
       />
     </div>
   );
