e09c2d9fb4af4e8c967b7ca74f8f1cb8c6f383a1
Joel Muniz <Jmuni017@ucr.edu>
2024-05-20 02:08:59 -0700

    Reworking animations

--- src/components/home/ProjectType.jsx
+++ src/components/home/ProjectType.jsx
@@ -1,26 +1,23 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 const ProjectType = ({ icon, title, description, className = " " }) => {
   return (
-    <motion.div
-      whileHover={{ rotateY: 360, scale: 1.125 }}
-      transition={{ duration: 1 }}
-      className={`m-4 p-4 w-80 h-80 rounded-xl shadow-md flex flex-col justify-start items-center gap-2 ${className} transform transition duration-100 hover:-translate-y-2`}
+    <div
+      className={`m-4 p-4 w-80 h-80 rounded-xl shadow-md flex flex-col justify-start items-center gap-2 ${className} transform transition duration-100`}
     >
       <Image
         src={icon}
         alt="Icon"
         className="w-1/2 h-1/4 rounded-xl object-cover overflow-visible mt-6"
       />
-
       <div className="bg-yellow-200 text-center mt-0 font-bold text-3xl text-white bg-clip-text ">
         {title}
         <div className="text-base mt-1 text-white">{description}</div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
--- src/components/home/ProjectTypes.jsx
+++ src/components/home/ProjectTypes.jsx
@@ -30,50 +30,85 @@ const Cards = () => {
         transition={{ type: "spring", stiffness: 500, delay: 0.4 }}
         className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-center"
       >
-        <ProjectType
-          icon={paintbrush}
-          title="Painting"
-          description="At ArtFactory, we incorporate the classic practice of painting into most of our events, 
-                      providing therapeutic experience for most art enjoyers."
-          className="bg-art-purple"
-        />
-        <ProjectType
-          icon={pencil}
-          title="Sketching"
-          description="Sketching plays a crucial role in most craft activities. With ArtFactory, 
-                      members will get to practice with the foundational step of creativity."
-          className="bg-art-purple-100"
-        />
-        <ProjectType
-          icon={hand}
-          title="Crafting"
-          description="Crafting activities are exciting ways to incorporate creativity with practicality. 
-                      Our club offers fun and interactive crafting events with supply included."
-          className="bg-art-purple"
-        />
+        <motion.div
+          whileHover={{
+            scale: 1.125,
+            transition: { type: "spring", stiffness: 600 },
+          }}
+        >
+          <ProjectType
+            icon={paintbrush}
+            title="Painting"
+            description="At ArtFactory, we incorporate the classic practice of painting into most of our events, 
+                        providing therapeutic experience for most art enjoyers."
+            className="bg-art-purple"
+          />
+        </motion.div>
+        <motion.div
+          whileHover={{
+            scale: 1.125,
+            transition: { type: "spring", stiffness: 600 },
+          }}
+        >
+          <ProjectType
+            icon={pencil}
+            title="Sketching"
+            description="Sketching plays a crucial role in most craft activities. With ArtFactory, 
+                        members will get to practice with the foundational step of creativity."
+            className="bg-art-purple-100"
+          />
+        </motion.div>
+        <motion.div
+          whileHover={{
+            scale: 1.125,
+            transition: { type: "spring", stiffness: 600 },
+          }}
+        >
+          <ProjectType
+            icon={hand}
+            title="Crafting"
+            description="Crafting activities are exciting ways to incorporate creativity with practicality. 
+                        Our club offers fun and interactive crafting events with supply included."
+            className="bg-art-purple"
+          />
+        </motion.div>
       </motion.div>
 
       <motion.div
         variants={animate}
         initial="start"
         whileInView="end"
-        transition={{ type: "spring", stiffness: 500, delay: 0.6 }}
+        transition={{ type: "spring", stiffness: 500, delay: 0.5 }}
         className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center"
       >
-        <ProjectType
-          icon={origami}
-          title="DIY"
-          description="DIY provides the manufacturing experience of our daily objects. With these activities, 
-                      members can have a sentimental connection with modern availability and appreciation for craftsmanship."
-          className="bg-art-purple"
-        />
-        <ProjectType
-          icon={Vector}
-          title="Studio Time"
-          description="Studio Art majors can take advantage of our studio time for their own projects, 
-                    while other members get hands-on comprehensive art experience to their fullest potential!"
-          className="bg-art-purple-100"
-        />
+        <motion.div
+          whileHover={{
+            scale: 1.125,
+            transition: { type: "spring", stiffness: 600 },
+          }}
+        >
+          <ProjectType
+            icon={origami}
+            title="DIY"
+            description="DIY provides the manufacturing experience of our daily objects. With these activities, 
+                        members can have a sentimental connection with modern availability and appreciation for craftsmanship."
+            className="bg-art-purple"
+          />
+        </motion.div>
+        <motion.div
+          whileHover={{
+            scale: 1.125,
+            transition: { type: "spring", stiffness: 600 },
+          }}
+        >
+          <ProjectType
+            icon={Vector}
+            title="Studio Time"
+            description="Studio Art majors can take advantage of our studio time for their own projects, 
+                      while other members get hands-on comprehensive art experience to their fullest potential!"
+            className="bg-art-purple-100"
+          />
+        </motion.div>
       </motion.div>
 
       <br></br>
@@ -81,7 +116,7 @@ const Cards = () => {
         variants={animate}
         initial="start"
         whileInView="end"
-        transition={{ type: "spring", stiffness: 500, delay: 0.8 }}
+        transition={{ type: "spring", stiffness: 500, delay: 0.6 }}
       >
         <motion.p
           whileHover={{
