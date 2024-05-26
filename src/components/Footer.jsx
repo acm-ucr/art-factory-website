d4823dfa9d438980411ba4424c4a3f4f5ad068f3
Brian Helewa <brianhelewa@ucr-secure-36-10-13-194-53.wnet.ucr.edu>
2024-05-25 21:10:29 -0700

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,18 +8,18 @@ import { RiTiktokLine } from "react-icons/ri";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between bg-art-purple-100 p-1">
-      <div className="flex items-center gap-3 ">
+    <div className="flex flex-col md:flex-row justify-between items-center bg-art-purple-100 p-4 md:p-6">
+      <div className="flex items-center gap-3 mb-4 md:mb-0">
         <Image src={paintbrush} alt="brush icon" className="scale-75" />
-        <div className="text-white font-nunito text-3xl font-semibold">
+        <div className="text-white font-nunito text-xl md:text-3xl font-semibold">
           Art Factory
         </div>
       </div>
-      <div className="flex gap-10 items-center mr-4">
-        <FaDiscord className="text-4xl text-white" />
-        <FaInstagram className="text-4xl text-white" />
-        <IoMailOutline className="text-4xl text-white" />
-        <RiTiktokLine className="text-4xl text-white" />
+      <div className="flex gap-4 md:gap-10 items-center justify-center w-full md:w-auto">
+        <FaDiscord className="text-2xl md:text-4xl text-white" />
+        <FaInstagram className="text-2xl md:text-4xl text-white" />
+        <IoMailOutline className="text-2xl md:text-4xl text-white" />
+        <RiTiktokLine className="text-2xl md:text-4xl text-white" />
       </div>
     </div>
   );
