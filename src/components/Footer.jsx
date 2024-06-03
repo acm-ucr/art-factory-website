e00a8873457c38657c1a2499180cf55ff70bf998
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-06-03 14:48:27 -0700

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import paintbrush from "@/public/paint.svg";
-import { FaDiscord } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaDiscord, FaInstagram } from "react-icons/fa";
 import { IoMailOutline } from "react-icons/io5";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-art-purple-100 p-4 md:p-6">
-      <div className="flex flex-col md:flex-row items-center gap-3 mb-4 md:mb-0">
+    <div className="flex items-center justify-between bg-art-purple-100 p-4 md:p-6 w-full">
+      <div className="flex items-center gap-3">
         <Image src={paintbrush} alt="brush icon" className="scale-75" />
-        <div className="text-white font-nunito text-xl md:text-3xl font-semibold mt-2 md:mt-0">
+        <div className="text-white font-nunito text-xl md:text-3xl font-semibold">
           Art Factory
         </div>
       </div>
