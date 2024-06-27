f2f1d80df1ed16095245d9f9a1540eb528a31c2d
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-06-27 15:53:02 -0700

    added board photos and gallery p1

--- src/data/board.js
+++ src/data/board.js
@@ -1,44 +1,50 @@
-import test from "../../public/jonathan2024.webp";
+import Adreyan from "@/public/board/Adreyan.JPG";
+import Lydia from "@/public/board/Lydia.JPG";
+import Pamela from "../../public/board/Pamela.jpeg";
+import Ricky from "@/public/board/Ricky.jpeg";
+import Rifah from "@/public/board/Rifah.jpeg";
+import Tran from "@/public/board/Tran.jpeg";
+
 export const Board = [
   {
     name: "Pamela Gomez",
     title: "President",
-    image: test,
+    image: Pamela,
     intro:
       "Hello! I am Pamela, the president and founder of Art Factory. I am a second year passionate about my major in psychology. I am from Coronado, San Diego. A fun fact about me is that i was a horse rider for a long time and know how to train them 🏇. I aspire to be a great therapist one day 🧠!",
   },
   {
     name: "Lydia Niu",
     title: "Vice President",
-    image: test,
+    image: Lydia,
     intro:
       " Hi~~I’m Lydia, I’m the vice president of the club. I am from Taiwan and am currently a second-year student majoring in Data Science. I have many hobbies, but the most surprising one is I can sing! Haha~~ To be specific, I sing classical opera. My ultimate goal is to complete Mozart’s The Magic Flute before graduation! (maybe it’ll cause me to never graduate jk",
   },
   {
     name: "Rifa Sanjidah",
     title: "Secretary",
-    image: test,
+    image: Rifah,
     intro:
       "Greetings! My name is Rifah and I’m the Secretary. I’m a 2nd year Neuroscience major from LA. Fun fact about me is that I can sleep for 12 hours and still manage to take a nap later on LOL. I aspire to be a good plant taker.",
   },
   {
     name: "Ricky Le",
     title: "Treasurer",
-    image: test,
+    image: Ricky,
     intro:
       "HELLO! My name is Ricky and I'm the treasure. I'm a 2nd-year biology major from Garden Grove. I handle our club’s finances, but I'm currently on the hunt for a fiance. If you are 6’2, rich, and preferably single then say less 😻 Fun fact about me is that I can do a handstand on top of a balloon  (in my dreams).",
   },
   {
     name: "Tran Nguyen",
     title: "Social Media",
-    image: test,
+    image: Tran,
     intro:
       "Hey! I’m Tran from the Art History Major. This is my third year at UCR. Like many of you guys, I love to rot in bed watching anime and play games, lol. Art has always been my passion and I am looking forward to sharing this journey with you guys.",
   },
   {
     name: "Adreyan Distor",
     title: "Social Media",
-    image: test,
+    image: Adreyan,
     intro:
       "YOOOO. I’m Adreyan, I’m a Data Science Major. With the other co-lead Tran, we run our club’s socials and everything. I like making 3D art, youtube videos, and making random coding projects. I like creating THINGS. The goal’s to make enough money so I can spend the rest of my life working on projects I care about! I also used to do puppetry when I was a kid and I made a 52 song album with my friends ✌️",
   },
